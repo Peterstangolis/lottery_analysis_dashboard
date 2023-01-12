ffd2a5e1670e5892bad5da279d389e2a6619a622
Peter Stangolis <pstangolis@gmail.com>
2023-01-12 16:49:16 -0500

    format changes

--- number_occurrence_chart.py
+++ number_occurrence_chart.py
@@ -27,7 +27,7 @@ def keno_number_count(df):
         labelbottom=False)  # labels along the bottom edge are off
 
     ax.hlines(y=my_range, xmin=df_sorted["start"], xmax=df_sorted["SELECTED TOTAL"],
-              color='lightgreen', alpha=0.5)
+              color='slategrey', alpha=0.5)
 
     ax.scatter(df_sorted["SELECTED TOTAL"], my_range,
                # color='skyblue',
