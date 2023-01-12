53409083a4b075c0e1e459c4b5d549cbdd4be234
Peter Stangolis <pstangolis@gmail.com>
2023-01-12 16:04:04 -0500

    format changes

--- main.py
+++ main.py
@@ -93,7 +93,6 @@ if check_password():
             # st.markdown(f"<p style='color:#A17512 ; font-size: 20px;'> {quick_picks[0]} , {quick_picks[1]} </p> ",
             #             unsafe_allow_html=True)
             if submitted:
-                st.success(f"Here are your {numbers} Quick Pick numbers")
                 st.balloons()
                 st.markdown(f"<p style='color:#A17512 ; font-size: 26px; border-style:solid;border-color:#02A161; text-align:center; padding:5px;'> {quick_picks} </p> ", unsafe_allow_html=True)
             else:
